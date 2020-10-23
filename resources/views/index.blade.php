8c0b0b5513ae20997ba63baaef6c13c9838f3ab1
wrompich94 <davesin94@gmail.com>
2020-10-22 23:12:58 -0600

    actualizacion index buscardor

---
+++ resources/views/index.blade.php
@@ -0,0 +1,115 @@
+@extends('plantilla')
+
+@section('seccion')
+
+
+ <!--final codigo menu-->
+ <!--<img src="https://www.economicarentaautos.com/img/galerias/hyundai-tucson.jpg"  
+ width="1817" height="800" "alt="">-->
+
+ <div class="d-flex justify-content-center">
+ <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+      <img class="d-block img-fluid" src="https://images4.alphacoders.com/967/thumb-1920-96758.jpg" style="width:1200px; height:600px" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block img-fluid" src="https://images.alphacoders.com/147/thumb-1920-147481.jpg" style="width:1200px; height:600px" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block img-fluid" src="https://images2.alphacoders.com/209/thumb-1920-209460.jpg" style="width:1200px; height:600px" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+ </div>
+
+<div class="container-fluid bg-primary">
+
+ 
+  <div class="page-header" >
+    <center>
+    <h1 style="color:white;"">NUESTRA FLOTA</h1>
+   </center>
+  </div>
+
+  
+</div>
+    <div>
+    <form class="form-inline ml-3 float-right">
+      <input class="form-control form-control-navbar" name="search" type="search" placeholder="Busca vehiculo" aria-label="Search">
+      <button class="btn btn-navbar bg-warning" type="submit">
+      buscar
+      </button>
+    </form>
+    </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+
+<div class="container-fluid">
+
+    @foreach ($cliente->chunk(4) as $separa) 
+
+            <div class="row">
+              @foreach ($separa as $item) 
+              <div class="col-sm-6 col-md-3">
+                <div class="float-right">
+                  <div class="card border-left-dark">
+               
+                  <img src="{{ $item->img}}" width="425" height="250" alt="..."> <!--imagen de vehiculo-->
+                    
+                  <div class="caption">
+                    <h1><strong> {{ $item->marca }} </strong></h1> <!--nombre de Vehiculo-->
+
+                    <p class="card-text"> {{ $item->detalle }} </p><!--Caracteristica de vehiculo-->
+                    <p class="card-text"> {{ $item->modelo}} </p><!--precio de vehiculo-->
+
+                    <div class="float-right">
+
+                
+                    <p><a href="{{Route('test',['valor'=>$item->id_vehiculo])}}" class="btn btn-danger"  name="obtener" role="button" >
+                      Detalle</a>
+                    </p>
+                    
+
+                    </div>
+                    
+                  </div>
+
+                </div>
+                </div>
+              </div>
+              @endforeach
+            </div>
+            <br>
+            @endforeach
+            
+              <!--final del catalogo de datos-->
+            </div>
+
+                     
+            
+
+  </div>  
+
+
+
+
+
+@endsection   
+
